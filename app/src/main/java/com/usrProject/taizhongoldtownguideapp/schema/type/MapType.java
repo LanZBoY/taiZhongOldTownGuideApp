e9ebf004a13182566d5471f7baf3d4676d143d93
WenTee <superjimalex@gmail.com>
2022-01-19 12:04:47 +0800

    還原

--- app/src/main/java/com/usrProject/taizhongoldtownguideapp/schema/type/MapType.java
+++ app/src/main/java/com/usrProject/taizhongoldtownguideapp/schema/type/MapType.java
@@ -6,7 +6,7 @@ public enum MapType {
     MAP_51(R.drawable.map_51,1.0f),
     MAP_1911(R.drawable.map_1911,1.0f),
     MAP_1937(R.drawable.map_1937,1.0f),
-    MAP_NOW(R.drawable.map_now_without_train,0.5f);
+    MAP_NOW(R.drawable.map_now,0.5f);
 
     public Integer currentScrollX;
     public Integer currentScrollY;
